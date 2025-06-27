44e048530a876d4b5b78672450481514b717ffb3
lmorgan2398 <lmorgan2398@gmail.com>
2025-06-27 02:59:34 +0000

    Implement merge sort function

--- src/index.js
+++ src/index.js
@@ -26,3 +26,55 @@ function fibsRecursive(n) {
     sequence.push(sequence[sequence.length - 1] + sequence[sequence.length - 2]);
     return sequence;
 }
+
+function mergeSort(array) {
+    // call recursive function on full array
+    mergeSortSplit(array, 0, array.length - 1);
+    // return sorted array
+    return array;
+}
+
+function mergeSortSplit(array, left, right) {
+    if (left >= right) 
+        return;
+    let middleSplit = Math.floor((left + right) / 2)
+    mergeSortSplit(array, left, middleSplit);
+    mergeSortSplit(array, middleSplit + 1, right);
+    mergeSortMerge(array, left, middleSplit, right);
+}
+
+function mergeSortMerge(array, left, middle, right) {
+    // store length of portion
+
+    // store both sorted array halves
+    let leftArray = array.slice(left, middle + 1);
+    let rightArray = array.slice(middle + 1, right + 1);
+
+    // i = left, j = right, k = merged
+    let i = 0, j = 0, k = left;
+    // Loop over all values in both arrays
+    while (i < leftArray.length && j < rightArray.length) {
+        // check left and right arrays and merge higher value
+        if(leftArray[i] < rightArray[j]) {
+            array[k] = leftArray[i];
+            k++;
+            i++;
+        } else {
+            array[k] = rightArray[j];
+            k++;
+            j++;
+        }
+        }
+        // once either array is sorted, continue through the other
+        while(i < leftArray.length) {
+            array[k] = leftArray[i];
+            k++;
+            i++;
+        }
+        while(j < rightArray.length) {
+            array[k] = rightArray[j];
+            k++;
+            j++
+        }
+    return;
+}
